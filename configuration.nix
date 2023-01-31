2b40f8dcbff1015f710b3e259723c998e4e9182e
Zack Lalanne <zack.lalanne@gmail.com>
2023-01-31 16:01:18 -0600

    Better fzf colors

--- configuration.nix
+++ configuration.nix
@@ -122,7 +122,14 @@
       enableAliases = true;
     };
 
-    programs.fzf = { enable = true; };
+    programs.fzf = {
+      enable = true;
+      defaultOptions = [
+        "--color=bg+:#073642,bg:#002b36,spinner:#719e07,hl:#586e75"
+        "--color=fg:#839496,header:#586e75,info:#cb4b16,pointer:#719e07"
+        "--color=marker:#719e07,fg+:#839496,prompt:#719e07,hl+:#719e07"
+      ];
+    };
 
     programs.git = {
       enable = true;
