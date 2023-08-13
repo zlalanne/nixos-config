8765784155d60a9ac7f8b751746e39fad204176c
Zack Lalanne <zack.lalanne@gmail.com>
2023-08-13 13:36:31 -0500

    Adding flash-nvim

--- dotfiles/nvim/lua/config/editor.lua
+++ dotfiles/nvim/lua/config/editor.lua
@@ -30,3 +30,10 @@ local bufremove = require("mini.bufremove")
 bufremove.setup({})
 vim.keymap.set("n", "<leader>bd", function() bufremove.delete(0, false) end, { desc = "Delete Buffer" })
 vim.keymap.set("n", "<leader>bD", function() bufremove.delete(0, true) end, { desc = "Delete Buffer (Force)" })
+
+-- Flash setup
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set({ "o" }, "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+vim.keymap.set({ "c" }, "r", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
