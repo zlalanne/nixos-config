0f707dce792982c0606582dc71cf20905f9797e2
Zack Lalanne <zack.lalanne@gmail.com>
2023-08-15 21:05:25 -0500

    Improve which-key bindings

--- dotfiles/nvim/lua/config/which-key.lua
+++ dotfiles/nvim/lua/config/which-key.lua
@@ -5,6 +5,24 @@ require("which-key").setup({
       enabled = true,
     },
   },
+  defaults = {
+    mode = { "n", "v" },
+    ["g"] = { name = "+goto" },
+    ["gz"] = { name = "+surround" },
+    ["]"] = { name = "+next" },
+    ["["] = { name = "+prev" },
+    ["<leader><tab>"] = { name = "+tabs" },
+    ["<leader>b"] = { name = "+buffer" },
+    ["<leader>c"] = { name = "+code" },
+    ["<leader>f"] = { name = "+file/find" },
+    ["<leader>g"] = { name = "+git" },
+    ["<leader>q"] = { name = "+quit/session" },
+    ["<leader>s"] = { name = "+search" },
+    ["<leader>u"] = { name = "+ui" },
+    ["<leader>w"] = { name = "+windows" },
+    ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    ["<leader>r"] = { name = "+references" },
+  },
 })
 
 local wk = require("which-key")
